463eea8fd61926aa3c8037f3bdd7c08b728d1fac
Ryan Zander <exide@hotmail.com>
2021-08-16 16:58:32 -0700

    Added  macos dependencies.

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,7 +1,17 @@
 if (WIN32)
     set(GLFW_ROOT "${DEPENDENCY_DIR}/windows/glfw-3.2.1")
+    find_library(
+            GLFW_LIBRARY
+            NAMES glfw3
+            PATHS "${GLFW_ROOT}/lib-mingw-w64"
+    )
 elseif (APPLE)
-    # TODO: add macos dependencies
+    set(GLFW_ROOT "${DEPENDENCY_DIR}/macos/glfw-3.3.4")
+    find_library(
+            GLFW_LIBRARY
+            NAMES glfw3
+            PATHS "${GLFW_ROOT}/lib-x86_64"
+    )
 else()
     # TODO: add linux dependencies
 endif()
@@ -10,10 +20,4 @@ find_path(
         GLFW_INCLUDE_DIR
         NAMES GLFW/glfw3.h
         PATHS "${GLFW_ROOT}/include"
-)
-
-find_library(
-        GLFW_LIBRARY
-        NAMES glfw3
-        PATHS "${GLFW_ROOT}/lib-mingw-w64"
 )
--- cmake/FindVulkan.cmake
+++ cmake/FindVulkan.cmake
@@ -1,19 +1,29 @@
 if (WIN32)
     set(VULKAN_ROOT "${DEPENDENCY_DIR}/windows/vulkan-1.1.85.0")
+    find_path(
+            VULKAN_INCLUDE_DIR
+            NAMES vulkan/vulkan.h
+            PATHS ${VULKAN_ROOT}/Include
+    )
+
+    find_library(
+            VULKAN_LIBRARY
+            NAMES vulkan-1
+            PATHS ${VULKAN_ROOT}/Lib ${VULKAN_ROOT}/Bin
+    )
 elseif (APPLE)
-    # TODO: add macos dependencies
+    set(VULKAN_ROOT "${DEPENDENCY_DIR}/macos/vulkan-1.2.182.0")
+    find_path(
+            VULKAN_INCLUDE_DIR
+            NAMES vulkan/vulkan.h
+            PATHS ${VULKAN_ROOT}/include
+    )
+
+    find_library(
+            VULKAN_LIBRARY
+            NAMES vulkan
+            PATHS ${VULKAN_ROOT}/lib ${VULKAN_ROOT}/bin
+    )
 else()
     # TODO: add linux dependencies
 endif()
-
-find_path(
-        VULKAN_INCLUDE_DIR
-        NAMES vulkan/vulkan.h
-        PATHS ${VULKAN_ROOT}/Include
-)
-
-find_library(
-        VULKAN_LIBRARY
-        NAMES vulkan-1
-        PATHS ${VULKAN_ROOT}/Lib ${VULKAN_ROOT}/Bin
-)
